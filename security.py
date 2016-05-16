d574b0dbadf52437399854654f75bd07addf0788
Jion Fairchild <jijiglobe@yahoo.com>
2016-05-15 23:10:37 -0400

    added empty security functions

--- security.py
+++ security.py
@@ -2,6 +2,22 @@ from passlib.hash import pbkdf2_sha256
 import pymongo
 from pymongo import MongoClient
 
+#Adds a user to database
+def addUser(username, password):
+    pass
+
+#Returns boolean that indicates whether a user has inputted correct login information
+def authenticate(username, password):
+    return False
+
+#Returns boolean indicating whether a certain username is taken
+def checkUsername(username):
+    return False
+
+#Removes user from the database
+def deleteUser(username):
+    pass
+
 # Hashes and returns password that is hashed and salted by 29000 rounds of pbkdf2 encryption                                          
 def hashPass(password):
     return pbkdf2_sha256.encrypt(password)
