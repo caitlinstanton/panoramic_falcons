ce35f2d756a9bd4f1ce1c14febef57c83ee5ad25
Jion Fairchild <jijiglobe@yahoo.com>
2016-05-20 10:31:28 -0400

    added log files, logging functions, and implemented logging into security functions

--- security.py
+++ security.py
@@ -1,14 +1,19 @@
+from logs import log
 from passlib.hash import pbkdf2_sha256
 import pymongo
 from pymongo import MongoClient
 
+#Runs full account creation script and returns message indicating operation status
 def createAccount(username, password, counselor, homeroom, firstName, lastName):
     if checkUsername(username,password):
-        return "Username in use!"
+        log(username, "attempted account creation")
+        return "Email in use"
     else:
         if addUser(username, password, counselor, homeroom, firstName, lastName):
+            log(username, "account created")
             return "Account creation successful"
         else:
+            log("sys","an unknown error accured during account creation")
             return "Account creation failed"
 
 #Adds a user to database
@@ -27,8 +32,13 @@ def addUser(username, password, counselor, homeroom, firstName, LastName):
 def authenticate(username, password):
     try:
         user = db.users.find({"name": username})
-        return verify(password,user[0]["hash"])
+        check = verify(password,user[0]["hash"])
+        if check:
+            log(username,"logged in")
+        else:
+            log(username,"failed login was attempted")
     except:
+        log(username, "an unknown user authentication error occurred")
         return False
 
 #Returns boolean indicating whether a certain username is taken
